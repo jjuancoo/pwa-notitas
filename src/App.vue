5363bfe55b274673f3d2a85b41542da382e01eda
Juanco <210373@utxicotepec.edu.mx>
2024-11-28 16:36:37 -0600

    Añade estilos a la vista principal

--- src/App.vue
+++ src/App.vue
@@ -71,3 +71,14 @@ export default {
   }),
 };
 </script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
