0ce2a5a6ee10755e86fb3160b1ce821acf31b06d
Juanco <210373@utxicotepec.edu.mx>
2024-11-28 17:14:36 -0600

    Crea listas

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -2,13 +2,30 @@
     <div class="hello">
         <h1>{{msg}}</h1>
     </div>
+
+    <div class="notes">
+        <ul>
+            <li v-for="(page, index) of pages" class="page">
+                <div> {{page.title}} titulo </div>
+                <div> {{page.content}} cont </div>
+            </li>
+            <li class="new-note">
+                <v-btn fab dark color="indigo" @click="newNote()">
+                    <v-icon dark>add</v-icon>
+                </v-btn>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default{
         name: 'Notes',
-        props: {
-            msg: String
+        props: ['pages'],
+        methods: {
+            newNote(){
+                this.$emit('new-note') 
+            }
         }
     }
 </script>
